4ee9ce93aa1d6f9fd08c483933e8715c0b569a60
PH <ph@retinavision.com.br>
2019-11-09 19:59:28 -0300

    api filter

--- api/filters.py
+++ api/filters.py
@@ -15,26 +15,37 @@ def filter_enquadramento(queryset, name, value):
 	return queryset
 
 class RadarFilter(django_filters.rest_framework.FilterSet):
+	codigo = django_filters.CharFilter(lookup_expr='icontains')
+	tipo_equip = django_filters.CharFilter(lookup_expr='icontains')
 	enquadrame = django_filters.CharFilter(method=filter_enquadramento)
+	velocidade = django_filters.CharFilter(lookup_expr='icontains')
 
 	class Meta:
 		model = BaseRadares
-		fields = '__all__'
+		fields = ['lote','codigo','tipo_equip','enquadrame','qtde_fxs_f','velocidade','ligado']
 
 
 class ContagensFilter(django_filters.rest_framework.FilterSet):
+	ano = django_filters.NumberFilter(field_name='data_e_hora', lookup_expr='year')
+	mes = django_filters.NumberFilter(field_name='data_e_hora', lookup_expr='month')
+	dia = django_filters.NumberFilter(field_name='data_e_hora', lookup_expr='day')
+
 	class Meta:
 		model = Contagens
-		fields = ['localidade', 'tipo']
+		fields = ['ano', 'mes', 'dia', 'localidade', 'tipo']
 
 
 class ViagensFilter(django_filters.rest_framework.FilterSet):
 	class Meta:
 		model = Viagens
-		fields = ['tipo']
+		fields = ['tipo','inicio','final']
 
 
 class TrajetosFilter(django_filters.rest_framework.FilterSet):
+
+	min_v0 = django_filters.NumberFilter(field_name="v0", lookup_expr='gte')
+	max_v0 = django_filters.NumberFilter(field_name="v0", lookup_expr='lte')
+
 	class Meta:
 		model = Trajetos
-		fields = ['viagem_id','tipo','origem','destino']
+		fields = ['viagem_id','tipo','origem','destino', 'min_v0','max_v0']
