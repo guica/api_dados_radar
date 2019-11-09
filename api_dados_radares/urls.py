7637ca183d5b643b32b3b0c973b34c06110ecc49
Guilherme <camargo.g95@hotmail.com>
2019-11-09 10:17:33 -0300

    Added Swagger view on /docs url

--- api_dados_radares/urls.py
+++ api_dados_radares/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(
+    title='Server Monitoring API',
+    url='/',
+    # patterns=schema_url_patterns,
+)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^docs/$', schema_view),
     url(r'^api/v1/',include('api.urls')),
     # url(r'^api/', include('api.urls')),
 
